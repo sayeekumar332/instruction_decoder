15eeee7c0ab3ddd7d592dbd705560f16fc6f8026
Sayeekumar1989 <sayeekumar.s@turing.com>
2025-12-12 20:17:54 +0530

    Golden Branch

--- sources/instruction_decoder.v
+++ sources/instruction_decoder.v
@@ -24,7 +24,7 @@ module instruction_decoder(
     // ----------------------------------------------------
     // Wires for decoder outputs
     // ----------------------------------------------------
-    wire cen_w [0:7];
+    /*wire cen_w [0:7];
     wire rst_w [0:7];
     wire oen_w [0:7];
     wire inc_w [0:7];
@@ -38,7 +38,28 @@ module instruction_decoder(
     wire src_sel_w  [0:7];
     wire stack_re_w [0:7];
     wire stack_we_w [0:7];
-    wire out_ce_w  [0:7];
+    wire out_ce_w  [0:7];*/
+    
+    // single-bit outputs: use packed 8-bit vectors (one bit per decoder)
+    wire [7:0] cen_w;
+    wire [7:0] rst_w;
+    wire [7:0] oen_w;
+    wire [7:0] inc_w;
+    wire [7:0] rsel_w;
+    wire [7:0] rce_w;
+    wire [7:0] pc_mux_sel_w;
+    wire [7:0] push_w;
+    wire [7:0] pop_w;
+    wire [7:0] src_sel_w;
+    wire [7:0] stack_re_w;
+    wire [7:0] stack_we_w;
+    wire [7:0] out_ce_w;
+
+    // two-bit buses: split into two bit-vectors (LSB and MSB)
+    wire [7:0] a_mux_sel_w0; // bit 0 for each decoder
+    wire [7:0] a_mux_sel_w1; // bit 1 for each decoder
+    wire [7:0] b_mux_sel_w0;
+    wire [7:0] b_mux_sel_w1;
 
     // ----------------------------------------------------
     // Instantiate all eight decoders
@@ -48,7 +69,7 @@ module instruction_decoder(
         .id(3'd0), .instr_in(instr_in), .cc_in(cc_in), .instr_en(instr_en),
         .cen(cen_w[0]), .rst(rst_w[0]), .oen(oen_w[0]), .inc(inc_w[0]),
         .rsel(rsel_w[0]), .rce(rce_w[0]), .pc_mux_sel(pc_mux_sel_w[0]),
-        .a_mux_sel(a_mux_sel_w[0]), .b_mux_sel(b_mux_sel_w[0]),
+        .a_mux_sel({a_mux_sel_w1[0], a_mux_sel_w0[0]}), .b_mux_sel({b_mux_sel_w1[0], b_mux_sel_w0[0]}),
         .push(push_w[0]), .pop(pop_w[0]), .src_sel(src_sel_w[0]),
         .stack_we(stack_we_w[0]),.stack_re(stack_re_w[0]), .out_ce(out_ce_w[0])
     );
@@ -57,7 +78,7 @@ module instruction_decoder(
         .id(3'd1), .instr_in(instr_in), .cc_in(cc_in), .instr_en(instr_en),
         .cen(cen_w[1]), .rst(rst_w[1]), .oen(oen_w[1]), .inc(inc_w[1]),
         .rsel(rsel_w[1]), .rce(rce_w[1]), .pc_mux_sel(pc_mux_sel_w[1]),
-        .a_mux_sel(a_mux_sel_w[1]), .b_mux_sel(b_mux_sel_w[1]),
+        .a_mux_sel({a_mux_sel_w1[1], a_mux_sel_w0[1]}), .b_mux_sel({b_mux_sel_w1[1], b_mux_sel_w0[1]}),
         .push(push_w[1]), .pop(pop_w[1]), .src_sel(src_sel_w[1]),
         .stack_we(stack_we_w[1]), .stack_re(stack_re_w[1]),.out_ce(out_ce_w[1])
     );
@@ -66,7 +87,7 @@ module instruction_decoder(
         .id(3'd2), .instr_in(instr_in), .cc_in(cc_in), .instr_en(instr_en),
         .cen(cen_w[2]), .rst(rst_w[2]), .oen(oen_w[2]), .inc(inc_w[2]),
         .rsel(rsel_w[2]), .rce(rce_w[2]), .pc_mux_sel(pc_mux_sel_w[2]),
-        .a_mux_sel(a_mux_sel_w[2]), .b_mux_sel(b_mux_sel_w[2]),
+        .a_mux_sel({a_mux_sel_w1[2], a_mux_sel_w0[2]}), .b_mux_sel({b_mux_sel_w1[2], b_mux_sel_w0[2]}),
         .push(push_w[2]), .pop(pop_w[2]), .src_sel(src_sel_w[2]),
         .stack_we(stack_we_w[2]), .stack_re(stack_re_w[2]) ,.out_ce(out_ce_w[2])
     );
@@ -75,7 +96,7 @@ module instruction_decoder(
         .id(3'd3), .instr_in(instr_in), .cc_in(cc_in), .instr_en(instr_en),
         .cen(cen_w[3]), .rst(rst_w[3]), .oen(oen_w[3]), .inc(inc_w[3]),
         .rsel(rsel_w[3]), .rce(rce_w[3]), .pc_mux_sel(pc_mux_sel_w[3]),
-        .a_mux_sel(a_mux_sel_w[3]), .b_mux_sel(b_mux_sel_w[3]),
+        .a_mux_sel({a_mux_sel_w1[3], a_mux_sel_w0[3]}), .b_mux_sel({b_mux_sel_w1[3], b_mux_sel_w0[3]}),
         .push(push_w[3]), .pop(pop_w[3]), .src_sel(src_sel_w[3]),
         .stack_we(stack_we_w[3]), .stack_re(stack_re_w[3]), .out_ce(out_ce_w[3])
     );
@@ -84,7 +105,7 @@ module instruction_decoder(
         .id(3'd4), .instr_in(instr_in), .cc_in(cc_in), .instr_en(instr_en),
         .cen(cen_w[4]), .rst(rst_w[4]), .oen(oen_w[4]), .inc(inc_w[4]),
         .rsel(rsel_w[4]), .rce(rce_w[4]), .pc_mux_sel(pc_mux_sel_w[4]),
-        .a_mux_sel(a_mux_sel_w[4]), .b_mux_sel(b_mux_sel_w[4]),
+        .a_mux_sel({a_mux_sel_w1[4], a_mux_sel_w0[4]}), .b_mux_sel({b_mux_sel_w1[4], b_mux_sel_w0[4]}),
         .push(push_w[4]), .pop(pop_w[4]), .src_sel(src_sel_w[4]),
         .stack_we(stack_we_w[4]), .stack_re(stack_re_w[4]),.out_ce(out_ce_w[4])
     );
@@ -93,7 +114,7 @@ module instruction_decoder(
         .id(3'd5), .instr_in(instr_in), .cc_in(cc_in), .instr_en(instr_en),
         .cen(cen_w[5]), .rst(rst_w[5]), .oen(oen_w[5]), .inc(inc_w[5]),
         .rsel(rsel_w[5]), .rce(rce_w[5]), .pc_mux_sel(pc_mux_sel_w[5]),
-        .a_mux_sel(a_mux_sel_w[5]), .b_mux_sel(b_mux_sel_w[5]),
+        .a_mux_sel({a_mux_sel_w1[5], a_mux_sel_w0[5]}), .b_mux_sel({b_mux_sel_w1[5], b_mux_sel_w0[5]}),
         .push(push_w[5]), .pop(pop_w[5]), .src_sel(src_sel_w[5]),
         .stack_we(stack_we_w[5]), .stack_re(stack_re_w[5]),.out_ce(out_ce_w[5])
     );
@@ -102,7 +123,7 @@ module instruction_decoder(
         .id(3'd6), .instr_in(instr_in), .cc_in(cc_in), .instr_en(instr_en),
         .cen(cen_w[6]), .rst(rst_w[6]), .oen(oen_w[6]), .inc(inc_w[6]),
         .rsel(rsel_w[6]), .rce(rce_w[6]), .pc_mux_sel(pc_mux_sel_w[6]),
-        .a_mux_sel(a_mux_sel_w[6]), .b_mux_sel(b_mux_sel_w[6]),
+        .a_mux_sel({a_mux_sel_w1[6], a_mux_sel_w0[6]}), .b_mux_sel({b_mux_sel_w1[6], b_mux_sel_w0[6]}),
         .push(push_w[6]), .pop(pop_w[6]), .src_sel(src_sel_w[6]),
         .stack_we(stack_we_w[6]), .stack_re(stack_re_w[6]),.out_ce(out_ce_w[6])
     );
@@ -111,7 +132,7 @@ module instruction_decoder(
         .id(3'd7), .instr_in(instr_in), .cc_in(cc_in), .instr_en(instr_en),
         .cen(cen_w[7]), .rst(rst_w[7]), .oen(oen_w[7]), .inc(inc_w[7]),
         .rsel(rsel_w[7]), .rce(rce_w[7]), .pc_mux_sel(pc_mux_sel_w[7]),
-        .a_mux_sel(a_mux_sel_w[7]), .b_mux_sel(b_mux_sel_w[7]),
+        .a_mux_sel({a_mux_sel_w1[7], a_mux_sel_w0[7]}), .b_mux_sel({b_mux_sel_w1[7], b_mux_sel_w0[7]}),
         .push(push_w[7]), .pop(pop_w[7]), .src_sel(src_sel_w[7]),
         .stack_we(stack_we_w[7]), .stack_re(stack_re_w[7]),.out_ce(out_ce_w[7])
     );
@@ -120,7 +141,7 @@ module instruction_decoder(
     // Output selection mux
     // ----------------------------------------------------
 
-    always @(*) begin
+        always @(*) begin
         cen        = cen_w[id];
         rst        = rst_w[id];
         oen        = oen_w[id];
@@ -128,8 +149,8 @@ module instruction_decoder(
         rsel       = rsel_w[id];
         rce        = rce_w[id];
         pc_mux_sel = pc_mux_sel_w[id];
-        a_mux_sel  = a_mux_sel_w[id];
-        b_mux_sel  = b_mux_sel_w[id];
+        a_mux_sel  = {a_mux_sel_w1[id], a_mux_sel_w0[id] };
+        b_mux_sel  = {b_mux_sel_w1[id], b_mux_sel_w0[id] };
         push       = push_w[id];
         pop        = pop_w[id];
         src_sel    = src_sel_w[id];
@@ -137,5 +158,4 @@ module instruction_decoder(
         stack_re   = stack_re_w[id];
         out_ce     = out_ce_w[id];
     end
-
 endmodule
