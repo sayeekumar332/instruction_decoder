85695a71b06b2ad1705bf459e4e7b799cb2b664d
Sayeekumar1989 <sayeekumar.s@turing.com>
2025-12-22 20:44:39 +0530

    Golden Solution

--- sources/instruction_decoder_5.v
+++ sources/instruction_decoder_5.v
@@ -21,21 +21,6 @@ module instruction_decoder_5(
     output reg out_ce
 );
 always @(*) begin : instruction_decoder_5
-   rst         = 1'b0;
-   out_ce      = 1'b0;
-   rsel        = 1'b0; 
-   rce         = 1'b0; 
-   cen         = 1'b0; 
-   stack_re    = 1'b0; 
-   pop         = 1'b0;
-   a_mux_sel   = 2'b10;
-   b_mux_sel   = 2'b10;
-   oen         = 1'b0; 
-   pc_mux_sel  = 1'b0; 
-   inc         = 1'b0; 
-   src_sel     = 1'b0;
-   push        = 1'b0;
-   stack_we    = 1'b0;
     // --------------------------------------
     // If ID != 101 → decoder is disabled
     // --------------------------------------
